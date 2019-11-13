809f65734ddbd141173b2e41988afbdb01feb7d2
Paul <paul.vangenuchten@geocat.net>
2019-11-13 23:01:20 +0100

    dutch skin translations

--- web-ui/src/main/resources/catalog/js/GnLocale.js
+++ web-ui/src/main/resources/catalog/js/GnLocale.js
@@ -87,7 +87,7 @@
         };
         var allPromises = [];
 
-        options.locales.push('custom');
+        options.locales.push('/../../catalog/views/dutch/locales/{{lang}}-core.json');
 
         angular.forEach(options.locales, function(value, index) {
           var langUrl = buildUrl(options.prefix, options.key,
